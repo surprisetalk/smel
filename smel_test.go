2037ea31a7d6786e6e3d757207bbde1eda841ceb
Taylor Troesh <taylor@troe.sh>
2025-02-17 19:01:19 -0800

    🍢

---
+++ smel_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"testing"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func TestModel_Update(t *testing.T) {
+	tests := []struct {
+		input  string
+		output string
+	}{
+		{
+			input:  "always (1 + 2 * 3 - 4)",
+			output: "123",
+		},
+		{
+			input:  "always 123",
+			output: "123",
+		},
+		{
+			input:  "123 + 1 |> always",
+			output: "124",
+		},
+		{
+			input:  "every 1000",
+			output: "TODO",
+		},
+		{
+			input:  "every 1000 |> random |> plot",
+			output: "TODO",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			m := model{
+				input: tt.input,
+			}
+			_, cmd := m.Update(tea.KeyMsg{Type: tea.KeyEnter})
+			msg := cmd().(evalResultMsg)
+			if msg.err != nil {
+				t.Errorf("error: %v", msg.err)
+			} else if msg.output != tt.output {
+				t.Errorf("expected output %s, got %s", tt.output, msg.output)
+			}
+		})
+	}
+}
