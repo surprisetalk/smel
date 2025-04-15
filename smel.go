a401404faa4b2016bb9c6e2497fda218bc30ac06
neuroevolutus <19356702+neuroevolutus@users.noreply.github.com>
2025-04-15 14:17:34 -0500

    Unnest `if`

--- smel.go
+++ smel.go
@@ -219,7 +219,11 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 											return m, nil
 										}
 
-											if op, ok := expr[2].(cbor.Tag); ok && op.Number == scrapscript.TagOp && op.Content == " " {
+											if op, ok = expr[2].(cbor.Tag); !ok || op.Number != scrapscript.TagOp || op.Content != " " {
+												m.err = fmt.Errorf("invalid cmd: %v", cmd)
+												return m, nil
+											}
+
 												if tag, ok := expr[0].(cbor.Tag); ok && tag.Number == scrapscript.TagTag {
 													switch tag.Content {
 													case "none":
@@ -244,10 +248,6 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 													m.err = fmt.Errorf("invalid cmd: %v", cmd)
 													return m, nil
 												}
-											} else {
-												m.err = fmt.Errorf("invalid cmd: %v", cmd)
-												return m, nil
-											}
 
 				// p.update = platform["update"].(scrapscript.Flat)
 
